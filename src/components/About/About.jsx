745db26f8cf484896f5ab3dfe7fdc759c00c75ac
Vivek-KA <vivekka6656@gmail.com>
2026-02-05 13:50:50 +0530

    fix: resolve image import issue for Vercel build

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/My image.jpeg';
+import profileImage from '../../assets/my-image.jpeg';
 
 const About = () => {
   return (
